6cbec453d4e38bb7b9fb2f4e9387f6e527d4cd50
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2020-09-16 00:12:59 -0300

    fixed typo

--- src/mtl/datasets/__init__.py
+++ src/mtl/datasets/__init__.py
@@ -3,7 +3,7 @@ import os
 classes = []
 for _file in os.listdir(os.path.dirname(__file__)):
     if _file != '__init__.py' and _file[-3:] == '.py':
-        file_name = module[:-3]
+        file_name = _file[:-3]
         classes.append(file_name)
 
 __all__ = classes
