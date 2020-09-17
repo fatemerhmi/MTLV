b26d8cd3916cdc9832d4a3c37d61bcc9aec58336
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2020-09-16 23:56:50 -0300

    read openI chexpert files and return them to configuration

--- src/mtl/datasets/openI.py
+++ src/mtl/datasets/openI.py
@@ -3,6 +3,7 @@ import torch
 import io
 import os
 from tqdm import tqdm
+import ast
 
 from mtl.datasets.utils import download_from_url, extract_archive, unicode_csv_reader
 
@@ -18,14 +19,18 @@ def _csv_iterator(data_path): # TODO: move this one to _create_data fater everyt
     with io.open(data_path, encoding="utf8") as f:
         reader = unicode_csv_reader(f)
         for row in reader:
-            text = ' '.join(row[1:])
-            yield int(row[0]) - 1, text
+            if row[0]=='text' and row[1]=="labels":
+                continue
+            # print(row)
+            text = row[0]
+            label = ast.literal_eval(row[1]) 
+            yield text, label
 
 def _create_data(iterator):
     data = []
     labels = []
     with tqdm(unit_scale=0, unit='lines') as t:
-        for cls, text in iterator:
+        for text, cls in iterator:
             data.append((cls, text))
             labels.append(cls)
             t.update(1)
@@ -55,7 +60,7 @@ class TextClassification(torch.utils.data.Dataset):
         inputs = self.tokenizer(sentecne, return_tensors="pt", **self.tokenizer_args)
         return (inputs, self._labels[i])
 
-    @property
+    # @property
     def __len__(self):
         return len(self._data)
 
@@ -74,13 +79,15 @@ class TextClassification(torch.utils.data.Dataset):
         return self._labels
 
 
-def _setup_datasets(dataset_name, tokenizer, tokenizer_args, root='.data'):
+def _setup_datasets(dataset_name, tokenizer, tokenizer_args, root='./data'):
     # check not to download if it already exists
+    # import ipdb
+    # ipdb.set_trace()
     if os.path.exists(f'{root}/{dataset_name}'): 
         #check if already preprocessed:
         if os.path.exists(f'{root}/{dataset_name}/cheXpertLabels'):
 
-            train_csv_path = f"{root}/{dataset_name}/cheXpertLabels/test.csv"
+            train_csv_path = f"{root}/{dataset_name}/cheXpertLabels/train.csv"
             print('[ _setup_dataset ] Creating training data')
             train_data, train_labels = _create_data(_csv_iterator(train_csv_path))
 
@@ -88,56 +95,39 @@ def _setup_datasets(dataset_name, tokenizer, tokenizer_args, root='.data'):
             print('[ _setup_dataset ] Creating testing data')
             test_data, test_labels = _create_data(_csv_iterator(test_csv_path))
             
-            val_csv_path = f"{root}/{dataset_name}/cheXpertLabels/test.csv"
+            val_csv_path = f"{root}/{dataset_name}/cheXpertLabels/val.csv"
             print('[ _setup_dataset ] Creating testing data')
             val_data, val_labels = _create_data(_csv_iterator(val_csv_path))
 
+            if len(set(train_labels[0]) ^ set(test_labels[0])) > 0:
+                raise ValueError("Training and test labels don't match")
+
+            return (TextClassification(train_data, train_labels, tokenizer, tokenizer_args),
+                    TextClassification(val_data, val_labels, tokenizer, tokenizer_args),
+                    TextClassification(test_data, test_labels, tokenizer, tokenizer_args))
 
     else: #dataset does not exists
         # download the datast:
-        dataset_tar = download_from_url(URLS[dataset_name], root=root)
+        dataset_tar = download_from_url("https://openi.nlm.nih.gov/imgs/collections/NLMCXR_reports.tgz", root=root)
 
         # preprocess the dataset:
+        print("[ openI dataset ] TODO: Finish this part")
+        # extracted_files = extract_archive(dataset_tar)
 
-    extracted_files = extract_archive(dataset_tar)
+        # for fname in extracted_files:
+        #     if fname.endswith('train.csv'):
+        #         train_csv_path = fname
+        #     if fname.endswith('test.csv'):
+        #         test_csv_path = fname
 
-    for fname in extracted_files:
-        if fname.endswith('train.csv'):
-            train_csv_path = fname
-        if fname.endswith('test.csv'):
-            test_csv_path = fname
+        # logging.info('Creating training data')
+        # train_data, train_labels = _create_data(_csv_iterator(train_csv_path))
 
-    logging.info('Creating training data')
-    train_data, train_labels = _create_data(_csv_iterator(train_csv_path))
+        # logging.info('Creating testing data')
+        # test_data, test_labels = _create_data(_csv_iterator(test_csv_path))
 
-    logging.info('Creating testing data')
-    test_data, test_labels = _create_data(_csv_iterator(test_csv_path))
+        # if len(set(train_labels) ^ set(test_labels)) > 0:
+        #     raise ValueError("Training and test labels don't match")
 
-    if len(set(train_labels) ^ set(test_labels)) > 0:
-        raise ValueError("Training and test labels don't match")
-
-    return (TextClassification(train_data, train_labels, tokenizer, tokenizer_args),
-            TextClassification(test_data, test_labels, tokenizer, tokenizer_args))
-
-
-# def OpenI(*args, **kwargs): #lower
-#     """ Defines AG_NEWS datasets.
-#         The labels includes:
-#             - 0 : World
-#             - 1 : Sports
-#             - 2 : Business
-#             - 3 : Sci/Tech
-#     Create supervised learning dataset: AG_NEWS
-#     Separately returns the training and test dataset
-#     Arguments:
-#         root: Directory where the datasets are saved. Default: ".data"
-#         ngrams: a contiguous sequence of n items from s string text.
-#             Default: 1
-#         vocab: Vocabulary used for dataset. If None, it will generate a new
-#             vocabulary based on the train data set.
-#         include_unk: include unknown token in the data (Default: False)
-#     Examples:
-#         >>> train_dataset, test_dataset = torchtext.datasets.AG_NEWS(ngrams=3)
-#     """
-
-#     return _setup_datasets(*(("AG_NEWS",) + args), **kwargs)
+        # return (TextClassification(train_data, train_labels, tokenizer, tokenizer_args),
+        #         TextClassification(test_data, test_labels, tokenizer, tokenizer_args))
