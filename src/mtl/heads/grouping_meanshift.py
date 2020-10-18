2e9a0fdb9bbbd454c5e57ccb631efb45fa8e0995
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2020-10-18 20:42:44 -0300

    added meanshift clustering algorithm for grouping

---
+++ src/mtl/heads/grouping_meanshift.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+
+__all__ = ['meanshift']
+
+def meanshift(x):
+  X = np.reshape(x, (-1, 1))
+  ms = MeanShift(bandwidth=None, bin_seeding=True)
+  ms.fit(X)
+  labels = ms.labels_
+  cluster_centers = ms.cluster_centers_
+
+  labels_unique = np.unique(labels)
+  n_clusters_ = len(labels_unique)
+
+  # print("number of estimated clusters : %d" % n_clusters_)
+  # print(labels)
+  heads_index = [list(np.where(labels==i)[0]) for i in range(n_clusters_)]
+  # print(heads_index)
+  # groupings = []
+  # for head_index in heads_index:
+  #   tmp = []
+  #   for i in head_index:
+  #     tmp.append(x[i])
+  #   groupings.append(tmp)
+  # print(groupings)
+  return heads_index
+
