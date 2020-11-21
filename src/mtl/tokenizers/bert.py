05e25b8856d4be7179bb0f3c45e6587c59851abb
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2020-11-21 10:51:27 -0500

    added directory location of model for tokenizer

--- src/mtl/tokenizers/bert.py
+++ src/mtl/tokenizers/bert.py
@@ -1,18 +1,28 @@
 from transformers import BertTokenizer
+import os
 
 __all__ = ['bert_base_uncased', 'bert_large_uncased', 'bert_base_cased', 'bert_large_cased']
 
 def bert_base_uncased():
-    return BertTokenizer.from_pretrained('bert-base-uncased')
-    
-def bert_large_uncased():
-    return BertTokenizer.from_pretrained('bert-base-uncased')
+    MODEL_PATH = "model_weights/bert-base-uncased"
+    if not os.path.exists(f"{MODEL_PATH}"):
+        return BertTokenizer.from_pretrained("bert-base-uncased")
+    else:
+        return BertTokenizer.from_pretrained(MODEL_PATH)
     
 def bert_base_cased():
-    return BertTokenizer.from_pretrained('bert-base-cased')
+    MODEL_PATH = "model_weights/bert-base-cased"
+    if not os.path.exists(f"{MODEL_PATH}"):
+        return BertTokenizer.from_pretrained("bert-base-cased")
+    else:
+        return BertTokenizer.from_pretrained(MODEL_PATH)
+
+def bert_large_uncased():
+    return BertTokenizer.from_pretrained('bert-large-uncased')
+    
     
 def bert_large_cased():
     return BertTokenizer.from_pretrained('bert-large-cased')
 
 
-#TODO: add fast tokenizer as well
+#TODO: add fast tokenizer 
