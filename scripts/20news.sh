47d4f503e0521dad2193f3ec58b809eda7000757
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2020-12-02 14:01:22 -0400

    removed 20news download script(it is included in the source code)

--- scripts/20news.sh
+++
@@ -1,5 +0,0 @@
-#download + unzip the file and remove the tar file after ward
-wget http://qwone.com/~jason/20Newsgroups/20news-bydate.tar.gz
-mkdir 20news-bydate
-tar -xvf 20news-bydate.tar.gz -C {data_dir}
-rm -r 20news-bydate.tar.gz
