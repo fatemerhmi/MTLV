223d27405ea052c66f35149643358aea1fcae14b
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2021-01-24 00:18:31 -0400

    added biobert tokenizers

--- src/mtl/tokenizers/bert.py
+++ src/mtl/tokenizers/bert.py
@@ -1,7 +1,10 @@
 from transformers import BertTokenizer
 import os
 
-__all__ = ['bert_base_uncased', 'bert_large_uncased', 'bert_base_cased', 'bert_large_cased']
+__all__ = ['bert_base_uncased', 'bert_large_uncased', \
+           'bert_base_cased', 'bert_large_cased',  \
+           'BioBERT_Basev1_1', 'BioBERT_Basev1_0_PM', \
+            'BioBERT_Basev1_0_PMC', 'BioBERT_Basev1_0_PM_PMC']
 
 def bert_base_uncased():
     MODEL_PATH = "model_weights/bert-base-uncased"
@@ -25,4 +28,18 @@ def bert_large_cased():
     return BertTokenizer.from_pretrained('bert-large-cased')
 
 
-#TODO: add fast tokenizer 
+def BioBERT_Basev1_1():
+    MODEL_PATH = "model_weights/biobert_v1.1_pubmed"
+    return BertTokenizer.from_pretrained(MODEL_PATH, return_dict=True)
+
+def BioBERT_Basev1_0_PM():
+    MODEL_PATH = "model_weights/biobert_v1.0_pubmed"
+    return BertTokenizer.from_pretrained(MODEL_PATH, return_dict=True)
+
+def BioBERT_Basev1_0_PMC():
+    MODEL_PATH = "model_weights/biobert_v1.0_pmc"
+    return BertTokenizer.from_pretrained(MODEL_PATH, return_dict=True)
+
+def BioBERT_Basev1_0_PM_PMC():
+    MODEL_PATH = "model_weights/biobert_v1.0_pubmed_pmc"
+    return BertTokenizer.from_pretrained(MODEL_PATH, return_dict=True)
