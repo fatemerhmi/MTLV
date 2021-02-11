3217095c27dbf991a19535ef89982c2e6a569ece
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2021-02-10 19:47:38 -0500

    added saving the model to further training

--- scripts/further_training.py
+++ scripts/further_training.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-df_train = pd.read_csv("../data/news/train.csv")
-df_test = pd.read_csv("../data/news/test.csv")
+df_train = pd.read_csv("data/news/train.csv")
+df_test = pd.read_csv("data/news/test.csv")
 
 df_train.replace(np.nan, "", inplace=True)
 df_test.replace(np.nan, "", inplace=True)
@@ -13,7 +13,7 @@ df_test.loc[:,'text'] = df_test.apply(lambda row: row['title']+" "+ row['desc'],
 train_txt = " ".join (df_train.text)
 test_txt = " ".join (df_test.text)
 
-SAVE_DIR = "../data/news"
+SAVE_DIR = "data/news"
 
 file1 = open(f"{SAVE_DIR}/train.txt","w")
 file1.write(train_txt) 
@@ -26,17 +26,15 @@ file2.close()
 from transformers import BertTokenizerFast
 from transformers import BertForMaskedLM
 
-tokenizer = BertTokenizerFast.from_pretrained(".../model_weights/bert-base-uncased/bert-base-uncased")
-
-
-model = BertForMaskedLM.from_pretrained('.../model_weights/bert-base-uncased/bert-base-uncased')
+tokenizer = BertTokenizerFast.from_pretrained("model_weights/bert-base-uncased")
+model = BertForMaskedLM.from_pretrained('model_weights/bert-base-uncased')
 
 #building the training dataset
 from transformers import LineByLineTextDataset
 
 dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path="../data/news/train.csv",
+    file_path="data/news/train.txt",
     block_size=128,
 )
 
@@ -50,7 +48,7 @@ data_collator = DataCollatorForLanguageModeling(
 from transformers import Trainer, TrainingArguments
 
 training_args = TrainingArguments(
-    output_dir="../model_weights/news_BERT",
+    output_dir="model_weights/news_BERT",
     overwrite_output_dir=True,
     num_train_epochs=1,
     per_device_train_batch_size=64,
@@ -66,3 +64,5 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+trainer.save_model("./model_weights/news_BERT")
