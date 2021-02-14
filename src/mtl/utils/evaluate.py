8d71cd0bb7ed188bdab28b0bad517d92fc0ecf6f
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2021-02-14 00:22:39 -0400

    renamed function and add threshold optimization

--- src/mtl/utils/evaluate.py
+++ src/mtl/utils/evaluate.py
@@ -2,23 +2,26 @@ from sklearn.metrics import classification_report, confusion_matrix, multilabel_
 import numpy as np
 from sklearn import metrics
 
-__all__ = ["calculate_f1_acc", "calculate_f1_acc_test", "hamming_score", "calculate_scores"]
+__all__ = ["calculate_scores_test", "calculate_scores"]
 
-def calculate_f1_acc(pred_labels, true_labels, threshold = 0.50):
-    target = true_labels
-    pred = np.array(pred_labels) >= 0.5
+# def calculate_f1_acc(pred_labels, true_labels, threshold = 0.50):
+#     target = true_labels
+#     pred = np.array(pred_labels) >= 0.5
 
-    f1_micro = f1_score(target,pred,average='micro')*100
-    f1_macro = f1_score(target,pred,average='macro')*100
-    subset_accuracy = accuracy_score(target, pred)*100
+#     f1_micro = f1_score(target,pred,average='micro')*100
+#     f1_macro = f1_score(target,pred,average='macro')*100
+#     subset_accuracy = accuracy_score(target, pred)*100
 
-    prf = precision_recall_fscore_support(target, pred, average=None)
-    LRAP = label_ranking_average_precision_score(target, pred_labels)
-    return f1_micro, f1_macro, acc, LRAP, prf
+#     prf = precision_recall_fscore_support(target, pred, average=None)
+#     LRAP = label_ranking_average_precision_score(target, pred_labels)
+#     return f1_micro, f1_macro, acc, LRAP, prf
 
 
-def calculate_scores(outputs, targets):
-    outputs = np.array(outputs) >= 0.5
+def calculate_scores(outputs, targets ): #threshold = 0.50
+    # outputs = np.array(outputs) >= 0.5
+    
+    threshold = calculate_f1_macro_opt(outputs, targets)
+    outputs = np.array(outputs) >= threshold
     
     f1_score_micro = metrics.f1_score(targets, outputs, average='micro')
     f1_score_micro = round(f1_score_micro*100,2)
@@ -40,9 +43,11 @@ def calculate_scores(outputs, targets):
     return f1_score_micro, f1_score_macro, hamming_loss_, hamming_score_, subset_accuracy, prf
 
 
-def calculate_f1_acc_test(pred_labels, true_labels, test_label_cols, threshold = 0.50):
+def calculate_scores_test(pred_labels, true_labels, test_label_cols):
+    threshold = calculate_f1_macro_opt(pred_labels, true_labels)
+
     target = true_labels
-    pred = np.array(pred_labels) >= 0.5
+    pred = np.array(pred_labels) >= threshold
     
     f1_score_micro = metrics.f1_score(target, pred, average='micro')
     f1_score_micro = round(f1_score_micro*100,2)
@@ -65,44 +70,42 @@ def calculate_f1_acc_test(pred_labels, true_labels, test_label_cols, threshold =
 
     return f1_score_micro, f1_score_macro, hamming_loss_, hamming_score_, subset_accuracy, clf_report
 
-def calculate_f1_acc_test_opt(pred_labels, true_labels, test_label_cols, threshold = 0.50):
+def calculate_f1_macro_opt(pred_labels, true_labels):
     # Calculate Accuracy - maximize F1 accuracy by tuning threshold values. First with 'macro_thresholds' on the order of e^-1 then with 'micro_thresholds' on the order of e^-2
-    print("-----Optimizing threshold value for micro F1 score-----")
 
     macro_thresholds = np.array(range(1,10))/10
 
-    f1_results, flat_acc_results = [], []
+    f1_results = []
     for th in macro_thresholds:
-        pred_bools = [pl>th for pl in pred_labels]
-        test_f1_accuracy = f1_score(true_bools,pred_bools,average='micro')
-        test_flat_accuracy = accuracy_score(true_bools, pred_bools)
+        # pred_bools = [pl>th for pl in pred_labels]
+        pred = np.array(pred_labels) >= th
+        test_f1_accuracy = f1_score(true_labels,pred,average='macro')
         f1_results.append(test_f1_accuracy)
-        flat_acc_results.append(test_flat_accuracy)
 
     best_macro_th = macro_thresholds[np.argmax(f1_results)] #best macro threshold value
+    return best_macro_th
 
-    micro_thresholds = (np.array(range(10))/100)+best_macro_th #calculating micro threshold values
+    # micro_thresholds = (np.array(range(10))/100)+best_macro_th #calculating micro threshold values
 
-    f1_results, flat_acc_results = [], []
-    for th in micro_thresholds:
-        pred_bools = [pl>th for pl in pred_labels]
-        test_f1_accuracy = f1_score(true_bools,pred_bools,average='micro')
-        test_flat_accuracy = accuracy_score(true_bools, pred_bools)
-        f1_results.append(test_f1_accuracy)
-        flat_acc_results.append(test_flat_accuracy)
+    # f1_results, flat_acc_results = [], []
+    # for th in micro_thresholds:
+    #     pred_bools = [pl>th for pl in pred_labels]
+    #     test_f1_accuracy = f1_score(true_bools,pred_bools,average='micro')
+    #     test_flat_accuracy = accuracy_score(true_bools, pred_bools)
+    #     f1_results.append(test_f1_accuracy)
+    #     flat_acc_results.append(test_flat_accuracy)
 
-    best_f1_idx = np.argmax(f1_results) #best threshold value
+    # best_f1_idx = np.argmax(f1_results) #best threshold value
 
-    # Printing and saving classification report
-    print('Best Threshold: ', micro_thresholds[best_f1_idx])
-    print('Test F1 Score: ', f1_results[best_f1_idx])
-    print('Test Accuracy: ', flat_acc_results[best_f1_idx])
-    print('LRAP: ', label_ranking_average_precision_score(true_labels, pred_labels) , '\n')
+    # # Printing and saving classification report
+    # print('Best Threshold: ', micro_thresholds[best_f1_idx])
+    # print('Test F1 Score: ', f1_results[best_f1_idx])
+    # print('Test Accuracy: ', flat_acc_results[best_f1_idx])
 
-    best_pred_bools = [pl>micro_thresholds[best_f1_idx] for pl in pred_labels]
-    clf_report_optimized = classification_report(true_bools,best_pred_bools, target_names=test_label_cols)
-    # pickle.dump(clf_report_optimized, open('classification_report_optimized.txt','wb'))
-    print(clf_report_optimized)
+    # best_pred_bools = [pl>micro_thresholds[best_f1_idx] for pl in pred_labels]
+    # clf_report_optimized = classification_report(true_bools,best_pred_bools, target_names=test_label_cols)
+    # # pickle.dump(clf_report_optimized, open('classification_report_optimized.txt','wb'))
+    # print(clf_report_optimized)
 
 def hamming_score(y_true, y_pred, normalize=True, sample_weight=None):
     acc_list = []
