f53758a197aa02fb057071780f54354332abe4ea
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2021-03-01 09:50:38 -0400

    aded a model for binary cls

--- src/mtl/models/bert.py
+++ src/mtl/models/bert.py
@@ -46,6 +46,75 @@ class BertCLS(BertPreTrainedModel):
         # pooled_output = self.dropout(pooled_output)
         return pooled_output
 
+class BertCLS_binarycls(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.hidden_size_BertCLS = config.hidden_size
+
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        # nn.init.xavier_normal_(self.classifier.weight)
+        self.init_weights()
+
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
+            Labels for computing the sequence classification/regression loss.
+            Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+            If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:            
+            loss_fct = BCEWithLogitsLoss() 
+            loss = loss_fct(logits.view(-1), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
 class BertCLS_multilabel_singleHead(BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
@@ -210,6 +279,7 @@ class BertCLS_multilabel_MTL(BertPreTrainedModel):
             attentions=outputs.attentions,
         )
 
+#------------------------dataset specific------------------------
 def bert_base_openI(num_labels, training_type, device='gpu'):
     MODEL_PATH = "model_weights/openI_BERT3"
 
@@ -246,6 +316,7 @@ def bert_base_news(num_labels, training_type, device='gpu'):
 
     return model
 
+#-----------------------general --------------------------------
 def bert_base_uncased(num_labels, training_type, device='gpu'):
     MODEL_PATH = "model_weights/bert-base-uncased"
     if not os.path.exists(MODEL_PATH):
@@ -259,6 +330,11 @@ def bert_base_uncased(num_labels, training_type, device='gpu'):
 
         elif training_type == "emb_cls":
             model = BertModel.from_pretrained('bert-base-uncased', return_dict=True)
+        
+        elif training_type == "STL_cls":
+            model =  BertCLS_binarycls.from_pretrained('bert-base-uncased', num_labels = 1, return_dict=True)
+            model.embedding_size = model.hidden_size_BertCLS
+
     
     else:
         if training_type == "singlehead_cls":
@@ -271,6 +347,10 @@ def bert_base_uncased(num_labels, training_type, device='gpu'):
 
         elif training_type == "emb_cls":
             model = BertModel.from_pretrained(MODEL_PATH, return_dict=True)
+        
+        elif training_type == "STL_cls":
+            model =  BertCLS_binarycls.from_pretrained(MODEL_PATH, num_labels = 1, return_dict=True)
+            model.embedding_size = model.hidden_size_BertCLS
 
     return model
 
