a0ec3e56d9c0582271c899416b533e9dc37044f3
Fatemeh Rahimi <fatemeh.rahimi@dal.ca>
2021-03-12 23:19:39 -0400

    fixed ttest with new training functions

--- src/main.py
+++ src/main.py
@@ -77,57 +77,60 @@ def run(config, gpu_id=0):
 
     model_cfg = cfg['model']
 
-    #================================ttest==================================
+    #================================ttest================================== between GMTL and MTL
     if training_type == "ttest":
         training_cv = True
         fold = 10
-        dataset_obj, dataset_name, dataset_args, tokenizer_obj, tokenizer_args, head_type, head_args, batch_size, model_cfg = configuration.setup_dataset(cfg['dataset'], cfg['tokenizer'], cfg['head'], cfg['model'], batch_size, training_cv, ttest = True)
-        results_MTL = []
-        results_singleHead = []
+        dataset_obj, dataset_name, dataset_args, tokenizer_obj, tokenizer_args, head_type, head_args, batch_size = configuration.setup_dataset(cfg['dataset'], cfg['tokenizer'], cfg['head'], cfg['model'], batch_size, training_cv, ttest = True)
+        results_MTL, results_GMTL = [], []
         fold_i = 0
 
-        for train_dataloader_s, val_dataloader_s, test_dataloader_s, num_labels_s, train_dataloader_mtl, val_dataloader_mtl, test_dataloader_mtl, num_labels_mtl in dataset_obj(dataset_name, dataset_args, tokenizer_obj, tokenizer_args, head_type, head_args, batch_size, model_cfg, fold): 
+        for train_dataloader_gmtl, validation_dataloader_gmtl, test_dataloader_gmtl, num_labels_gmtl, train_dataloader_mtl, val_dataloader_mtl, test_dataloader_mtl, num_labels_mtl in dataset_obj(dataset_name, dataset_args, tokenizer_obj, tokenizer_args, head_type, head_args, batch_size, model_cfg, fold): 
             fold_i += 1
             print(f"[main] Fold {fold_i}")
+            training_type_experiment = cfg['training']['type']
 
-            #-------single head--------
-            model_singleHead = configuration.setup_model(cfg['model'])(num_labels_s, "singlehead_cls")
-            test_f1_micro, test_f1_macro, test_subset_accuracy, test_hamming_score_ = train(train_dataloader_s, val_dataloader_s, test_dataloader_s, model_singleHead, cfg , use_cuda, "singlehead_cls", fold_i)
-            results_singleHead.append([test_f1_micro, test_f1_macro, test_hamming_score_, test_subset_accuracy])
+            #--------MTL
+            test_f1_micro, test_f1_macro, test_hamming_score_, test_subset_accuracy  = MTL(train_dataloader_mtl, val_dataloader_mtl, test_dataloader_mtl, num_labels_mtl, cfg, use_cuda, fold_i, training_type_experiment)
+            results_MTL.append([test_f1_micro, test_f1_macro, test_hamming_score_, test_subset_accuracy])
+
+            #-------GMTL
+            test_f1_micro, test_f1_macro, test_subset_accuracy, test_hamming_score_ = GMTL(train_dataloader_gmtl, validation_dataloader_gmtl, test_dataloader_gmtl, cfg , use_cuda, fold_i)
+            results_GMTL.append([test_f1_score_micro, test_f1_score_macro, test_hamming_score_, test_subset_accuracy])
+
+
+            # #-------single head--------
+            # model_singleHead = configuration.setup_model(cfg['model'])(num_labels_s, "singlehead_cls")
+            # test_f1_micro, test_f1_macro, test_subset_accuracy, test_hamming_score_ = train(train_dataloader_s, val_dataloader_s, test_dataloader_s, model_singleHead, cfg , use_cuda, "singlehead_cls", fold_i)
             
-            #-------MTL----------------
-            heads_index = ast.literal_eval(mlflowLogger.get_params("heads_index"))
-            num_labels_head = [len(labels) for labels in heads_index]
-            model_MTL = configuration.setup_model(cfg['model'])(num_labels_head, "MTL_cls", device)
-            test_f1_score_micro, test_f1_score_macro, test_hamming_score_, test_subset_accuracy = train(train_dataloader_mtl, val_dataloader_mtl, test_dataloader_mtl, model_MTL, cfg , use_cuda, "MTL_cls", fold_i)
-            results_MTL.append([test_f1_score_micro, test_f1_score_macro, test_hamming_score_, test_subset_accuracy])
+            # #-------MTL----------------
+            # heads_index = ast.literal_eval(mlflowLogger.get_params("heads_index"))
+            # num_labels_head = [len(labels) for labels in heads_index]
+            # model_MTL = configuration.setup_model(cfg['model'])(num_labels_head, "MTL_cls", device)
+            # test_f1_score_micro, test_f1_score_macro, test_hamming_score_, test_subset_accuracy = train(train_dataloader_mtl, val_dataloader_mtl, test_dataloader_mtl, model_MTL, cfg , use_cuda, "MTL_cls", fold_i)
 
 
-        #-------calculate mean and variance of run details------
-        # mlflowLogger.store_metric(f"results_MTL", results_MTL)
-        # mlflowLogger.store_metric(f"results_singleHead", results_singleHead)
-
-        # ttest
+        #------- ttest, pvalue, ... ----------
         results_MTL = np.array(results_MTL)
         store_mflow_metrics(results_MTL, "MTL")
-        results_singleHead = np.array(results_singleHead)
-        store_mflow_metrics(results_singleHead, "STL")
+        results_GMTL = np.array(results_GMTL)
+        store_mflow_metrics(results_GMTL, "GMTL")
 
-        _, ttest_f1_mi =  stats.ttest_rel(results_MTL[:,0],results_singleHead[:,0])
-        _, ttest_f1_ma =  stats.ttest_rel(results_MTL[:,1],results_singleHead[:,1])
-        _, ttest_hammingscore   =  stats.ttest_rel(results_MTL[:,2],results_singleHead[:,2])
-        _, ttest_subset_acc   =  stats.ttest_rel(results_MTL[:,3],results_singleHead[:,3])
+        _, ttest_f1_mi =  stats.ttest_rel(results_MTL[:,0],results_GMTL[:,0])
+        _, ttest_f1_ma =  stats.ttest_rel(results_MTL[:,1],results_GMTL[:,1])
+        _, ttest_hammingscore   =  stats.ttest_rel(results_MTL[:,2],results_GMTL[:,2])
+        _, ttest_subset_acc   =  stats.ttest_rel(results_MTL[:,3],results_GMTL[:,3])
 
         mlflowLogger.store_metric(f"ttest_f1_mi", ttest_f1_mi)          
         mlflowLogger.store_metric(f"ttest_f1_ma", ttest_f1_ma)          
         mlflowLogger.store_metric(f"ttest_hammingscore", ttest_hammingscore)          
         mlflowLogger.store_metric(f"ttest_subset_acc", ttest_subset_acc)          
 
-        # ttest wilcoxon
-        _, ttest_f1_mi =  stats.wilcoxon(results_MTL[:,0],results_singleHead[:,0])
-        _, ttest_f1_ma =  stats.wilcoxon(results_MTL[:,1],results_singleHead[:,1])
-        _, ttest_hammingscore   =  stats.wilcoxon(results_MTL[:,2],results_singleHead[:,2])
-        _, ttest_subset_acc   =  stats.wilcoxon(results_MTL[:,3],results_singleHead[:,3])
+        #---------ttest wilcoxon
+        _, ttest_f1_mi =  stats.wilcoxon(results_MTL[:,0],results_GMTL[:,0])
+        _, ttest_f1_ma =  stats.wilcoxon(results_MTL[:,1],results_GMTL[:,1])
+        _, ttest_hammingscore   =  stats.wilcoxon(results_MTL[:,2],results_GMTL[:,2])
+        _, ttest_subset_acc   =  stats.wilcoxon(results_MTL[:,3],results_GMTL[:,3])
 
         mlflowLogger.store_metric(f"ttest_f1_wilcoxon_mi", ttest_f1_mi)          
         mlflowLogger.store_metric(f"ttest_f1_wilcoxon_ma", ttest_f1_ma)          
@@ -159,11 +162,6 @@ def run(config, gpu_id=0):
             
             elif training_type == "GMHL_cls":
                 test_f1_micro, test_f1_macro, test_subset_accuracy, test_hamming_score_ = GMHL(train_dataloader, validation_dataloader, test_dataloader, cfg , use_cuda, fold_i)
-
-            # if training_type == "singlehead_cls":
-            #     model = configuration.setup_model(cfg['model'])(num_labels, training_type)
-            #     test_f1_micro, test_f1_macro, test_hamming_score_, test_subset_accuracy = train(train_dataloader, val_dataloader, test_dataloader, model, cfg , use_cuda, training_type, fold_i)
-
 
             results.append([test_f1_micro, test_f1_macro, test_hamming_score_, test_subset_accuracy])
         
